cfbc0e6b7bf2f1da6349a158cf7c626a36a05eb9
jihzanwei <670241885@qq.com>
2019-09-28 09:13:12 +0800

    111

--- app/Http/Controllers/kaoshi/KaoshiController.php
+++ app/Http/Controllers/kaoshi/KaoshiController.php
@@ -8,37 +8,37 @@ use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\File;
 use DB;
-use phpDocumentor\Reflection\Location;;
-
+use phpDocumentor\Reflection\Location;
+use App\Http\Controllers\Event;
 /**
- * 
+ *
  */
 class kaoshiController extends Controller
 {
-	
-	
-		public function token()
-			{
 
-			    $redis = new \Redis();
-			    $redis->connect('127.0.0.1','6379');
-			    //加入缓存
-			    $access_token_key = 'wechat_access_token';
-			    if($redis->exists($access_token_key)){
-			        //存在
-			        return $redis->get($access_token_key);
-			    }else{
-			        //不存在
-			        $result = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WECHAT_APPID').'&secret='.env('WECHAT_APPSECRET'));
-			        $re = json_decode($result,1);
-			        $redis->set($access_token_key,$re['access_token'],$re['expires_in']);  //加入缓存
-			        return $re['access_token'];
-			    }
-			    // dd($token);
 
-			}
+    public function token()
+    {
 
-	
+        $redis = new \Redis();
+        $redis->connect('127.0.0.1','6379');
+        //加入缓存
+        $access_token_key = 'wechat_access_token';
+        if($redis->exists($access_token_key)){
+            //存在
+            return $redis->get($access_token_key);
+        }else{
+            //不存在
+            $result = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WECHAT_APPID').'&secret='.env('WECHAT_APPSECRET'));
+            $re = json_decode($result,1);
+            $redis->set($access_token_key,$re['access_token'],$re['expires_in']);  //加入缓存
+            return $re['access_token'];
+        }
+        // dd($token);
+
+    }
+
+
 
 
 }
